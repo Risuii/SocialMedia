1e2e967ca8df1bccf36c050de26301bdbcc69075
Risuii <farisfhm97@gmail.com>
2022-04-12 21:32:35 +0700

    done

--- config/config.js
+++ config/config.js
@@ -12,5 +12,31 @@ module.exports={
         "rejectUnauthorized":false
       }
     }
+  },
+  "test": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require":true,
+        "rejectUnauthorized":false
+      }
+    }
+  },
+  "production": {
+    "username": process.env.DB_USERNAME,
+    "password": process.env.DB_PASSWORD,
+    "database": process.env.DB_NAME,
+    "host": process.env.DB_HOST,
+    "dialect": "postgres",
+    "dialectOptions": {
+      "ssl": {
+        "require":true,
+        "rejectUnauthorized":false
+      }
+    }
   }
 }
